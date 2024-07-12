052b32c2f043cdc40d78733878d9d0cb0e4f4878
itsLydt <lklaus.dev@gmail.com>
2024-07-12 11:35:11 -0500

    fixed author

---
+++ drivers/spi/spi.c
@@ -0,0 +1,127 @@
+/*
+ * spi.c
+ *
+ * Created: 7/2/2024 12:04:20 PM
+ *  Author: itsLydt
+ */ 
+
+#include "spi.h"
+
+/*
+SERCOM number: 0-5
+setClocksEnabled: true to enable, false to disable
+clkGenerator: select clock generator, 0-8
+Note that clock generator must be configured separately
+*/
+void SPI_ClkControl(uint8_t sercom_num, bool setBusClockEnabled, uint8_t serialClkGenerator){
+	// enable/disable APBC bus clock and peripheral clock for specified SERCOM instance
+	
+	uint32_t bus_mask = (0x01 << (2 + sercom_num)); // SERCOM0: bit2, SERCOM1: bit3, etc
+	uint32_t clk_ctrl = GCLK_CLKCTRL_GEN(serialClkGenerator) | GCLK_CLKCTRL_ID(0x14 + sercom_num); // SERCOM0: ID=14, SERCOM1: ID:15, etc
+
+	if(setBusClockEnabled){
+		PM->APBCMASK.reg |= bus_mask;
+	}
+	else {
+		PM->APBCMASK.reg &= ~bus_mask;
+	}
+	
+	if(serialClkGenerator != -1)
+		GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | clk_ctrl;
+	else 
+		GCLK->CLKCTRL.reg = clk_ctrl;
+}
+
+void SPI_commonInit(Sercom* sercom, bool isMaster, uint8_t transfer_mode){
+	sercom->SPI.CTRLA.bit.MODE = isMaster? SERCOM_SPI_CTRLA_MODE_SPI_MASTER_Val : SERCOM_SPI_CTRLA_MODE_SPI_SLAVE_Val;
+	sercom->SPI.CTRLA.bit.DOPO = 0;  //PAD[0] Data out, PAD[1] SCK, PAD[2] SS
+	sercom->SPI.CTRLA.bit.DIPO = 3;  //PAD[3] Data in
+	sercom->SPI.CTRLA.bit.DORD = 0; // MSB first
+
+	sercom->SPI.CTRLA.bit.CPHA = transfer_mode % 2;
+	sercom->SPI.CTRLA.bit.CPOL = transfer_mode >= 2? 1 : 0;
+
+	sercom->SPI.CTRLB.bit.CHSIZE = 0; //chars are 8 bits
+}
+
+// baud: value calculated via f_ref/(2*f_baud) - 1, where f_ref is speed of serial clock (GCLK_SERCOM0_CORE) and f_baud is desired baud rate
+// AKA f_baud = f_ref /(2*(baud+1))
+// f_baud must be <= 0.5*f_ref
+void SPI_InitMaster(Sercom* sercom, uint8_t transfer_mode, uint8_t baud, bool hardwareSS){
+	SPI_SetEnabled(sercom, false);			// disable SERCOMx so configuration is possible
+
+	SPI_commonInit(sercom, true, transfer_mode);
+
+	sercom->SPI.BAUD.reg = SERCOM_SPI_BAUD_BAUD(baud);
+	sercom->SPI.CTRLB.bit.MSSEN = hardwareSS? 1 : 0;
+	
+	sercom->SPI.CTRLB.bit.RXEN = 1;			// enable receiver
+	SPI_SetEnabled(sercom, true);			// enable peripheral
+}
+
+// if address mode is -1, address matching will be turned off
+void SPI_InitSlave(Sercom* sercom, uint8_t transfer_mode, int8_t addressMode, uint8_t address, uint8_t addressMask){
+	SPI_SetEnabled(sercom, false);			// disable SERCOMx so configuration is possible
+
+	SPI_commonInit(sercom, false, transfer_mode);
+
+	if(addressMode == -1){
+		sercom->SPI.CTRLA.bit.FORM = 0; // normal SPI frame
+	}
+	else {
+		sercom->SPI.CTRLA.bit.FORM = 2; // SPI frame with address
+		sercom->SPI.CTRLB.bit.AMODE = addressMode;
+		sercom->SPI.ADDR.reg = SERCOM_SPI_ADDR_ADDRMASK(addressMask) | SERCOM_SPI_ADDR_ADDR(address);
+	}
+	sercom->SPI.CTRLB.bit.SSDE = 1;
+
+	sercom->SPI.CTRLB.bit.RXEN = 1;			// enable receiver
+	SPI_SetEnabled(sercom, true);			// enable peripheral
+	
+}
+
+
+void SPI_Reset(Sercom* sercom){
+	sercom->SPI.CTRLA.bit.SWRST = 1; // resets all registers of the sercom peripheral and disables it
+	while(sercom->SPI.SYNCBUSY.bit.SWRST);
+}
+
+void SPI_SetEnabled(Sercom* sercom, bool setEnabled){
+	sercom->SPI.CTRLA.bit.ENABLE = setEnabled? 1 : 0;
+	while(sercom->SPI.SYNCBUSY.bit.ENABLE);
+}
+
+
+//TODO: client preloading?
+void SPI_SendData(Sercom* spi, uint8_t* txBuffer, uint8_t* rxBuffer, size_t len){
+	for(int i = 0; i < len; i++){
+		//wait for tx buffer to empty
+		while(spi->SPI.INTFLAG.bit.DRE == 0); //TODO: WDT
+		//load DR with next byte
+		spi->SPI.DATA.reg = txBuffer[i];
+		
+		//wait for transmit complete
+		while(spi->SPI.INTFLAG.bit.TXC == 0); 
+
+		// wait for rx buffer to be full
+		while(spi->SPI.INTFLAG.bit.RXC == 0);
+		//read data 
+		rxBuffer[i] = spi->SPI.DATA.reg;
+	}
+	// wait for transmit to complete
+	while(spi->SPI.INTFLAG.bit.TXC == 0); 
+}
+
+uint32_t SPI_ReceiveData(Sercom* spi, uint8_t* rxBuffer, size_t toRead){
+	uint32_t bytesRead = 0;
+	// transmission is not complete
+	while(spi->SPI.INTFLAG.bit.TXC == 0 && bytesRead < toRead){
+		// wait for rx buffer to be full
+		while(spi->SPI.INTFLAG.bit.RXC == 0);
+		
+		rxBuffer[bytesRead] = spi->SPI.DATA.reg;
+		++bytesRead;
+	}
+	
+	return bytesRead;
+}
---
+++ drivers/spi/spi.h
@@ -0,0 +1,37 @@
+/*
+ * spi.h
+ *
+ * Created: 7/2/2024 12:04:07 PM
+ *  Author: itsLydt
+ */ 
+
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "sam.h"
+
+void SPI_ClkControl(uint8_t sercom_num, bool setClocksEnabled, uint8_t clkGenerator);
+
+void SPI_InitMaster(Sercom* sercom, uint8_t transfer_mode, uint8_t baud, bool hardwareSS);
+void SPI_InitSlave(Sercom* sercom, uint8_t transfer_mode, int8_t addressMode, uint8_t address, uint8_t addressMask);
+
+void SPI_Reset(Sercom* sercom);
+void SPI_SetEnabled(Sercom* sercom, bool setEnabled);
+
+
+/* Blocking calls for transmit and receive */
+void SPI_SendData(Sercom* spi, uint8_t* txBuffer, uint8_t* rxBuffer, size_t len);
+uint32_t SPI_ReceiveData(Sercom* spi, uint8_t* rxBuffer, size_t toRead);
+
+void SPI_ConfigureInterupt();
+void SPI_EnableInterrupt();
+void SPI_DisableInterrupt();
+
+//void SPI_LockConfiguration();
+
+#endif /* SPI_H_ */
