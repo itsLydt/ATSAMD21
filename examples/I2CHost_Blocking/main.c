e160986569cd62c5f00eb6f02a9aa2089cc7ad91
itsLydt <lklaus.dev@gmail.com>
2024-07-15 13:27:44 -0500

    Stub for i2c example project

---
+++ examples/I2CHost_Blocking/main.c
@@ -0,0 +1,19 @@
+/*
+ * I2CHost_Blocking.c
+ *
+ * Created: 7/15/2024 1:21:03 PM
+ * Author : itsLydt
+ */ 
+
+
+#include "sam.h"
+
+#include "i2c.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
