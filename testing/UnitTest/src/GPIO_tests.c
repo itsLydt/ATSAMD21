a73e16c3240f122ef036782a19c0707722911bd4
itsLydt <lklaus.dev@gmail.com>
2024-08-05 17:12:14 -0500

    implemented some gpio related unit tests

--- testing/UnitTest/src/GPIO_tests.c
+++ testing/UnitTest/src/GPIO_tests.c
@@ -7,10 +7,132 @@
 #include "gpio_tests.h"
 #include "gpio.h"
 
-void test_myPassingTest(void){
-	TEST_ASSERT_EQUAL(0, 0);
+void t_pinToMask(void){
+	for(int pin = 0; pin < 32; pin++){
+		TEST_ASSERT_BIT_HIGH(pin, PIN_TO_MASK(pin));
+	}
 }
 
-void test_myFailingTest(void){
-	TEST_ASSERT_EQUAL(0, 100);
+// port A does not implement pins 26 and 29
+// port B does not implement pins 18, 19, 20, 21, 24, 25, 26, 27, 28, and 29
+void t_setPinsAsOutput(void){
+	const uint32_t implementedPins[3] = PORT_PIN_IMPLEMENTED;
+	
+	PortGroup* port = GPIOA;
+	char pinStr[3];
+	for(int pin = 0; pin < 32; pin++){
+		if(pin == 26 || pin == 29)
+			continue;
+		itoa(pin, pinStr, 10);
+		TEST_ASSERT_BIT_LOW_MESSAGE(pin, port->DIR.reg, pinStr);
+		GPIO_SetPinDirection(port, pin, GPIO_OUT);
+		TEST_ASSERT_BIT_HIGH_MESSAGE(pin, port->DIR.reg, pinStr);
+	}
+	TEST_ASSERT_BITS_HIGH(implementedPins[0], port->DIR.reg);
+
+	
+	port = GPIOB;
+	const int maxIndex = 10;
+	int pinsToSkip[10] = { 18, 19, 20, 21, 24, 25, 26, 27, 28, 29};
+	int skipIndex = 0;
+	for(int pin = 0; pin < 32; pin++){
+		while(skipIndex < maxIndex && pinsToSkip[skipIndex] < pin){
+			++skipIndex;
+		}
+		if(pin == pinsToSkip[skipIndex])
+			continue;
+		itoa(pin, pinStr, 10);
+		TEST_ASSERT_BIT_LOW_MESSAGE(pin, port->DIR.reg, pinStr);
+		GPIO_SetPinDirection(port, pin, GPIO_OUT);
+		TEST_ASSERT_BIT_HIGH_MESSAGE(pin, port->DIR.reg, pinStr);
+	}
+	
+	TEST_ASSERT_BITS_HIGH(implementedPins[1], port->DIR.reg);
+};
+
+void t_setPinsAsInput(void){
+	const uint32_t implementedPins[3] = PORT_PIN_IMPLEMENTED;
+	
+	PortGroup* port = GPIOA;
+	char pinStr[3];
+	for(int pin = 0; pin < 32; pin++){
+		if(pin == 26 || pin == 29)
+			continue;
+		itoa(pin, pinStr, 10);
+		TEST_ASSERT_BIT_HIGH_MESSAGE(pin, port->DIR.reg, pinStr);
+		GPIO_SetPinDirection(port, pin, GPIO_IN);
+		TEST_ASSERT_BIT_LOW_MESSAGE(pin, port->DIR.reg, pinStr);
+	}
+	TEST_ASSERT_BITS_LOW(implementedPins[0], port->DIR.reg);
+
+	
+	port = GPIOB;
+	const int maxIndex = 10;
+	int pinsToSkip[10] = { 18, 19, 20, 21, 24, 25, 26, 27, 28, 29};
+	int skipIndex = 0;
+	for(int pin = 0; pin < 32; pin++){
+		while(skipIndex < maxIndex && pinsToSkip[skipIndex] < pin){
+			++skipIndex;
+		}
+		if(pin == pinsToSkip[skipIndex])
+			continue;
+		itoa(pin, pinStr, 10);
+		TEST_ASSERT_BIT_HIGH_MESSAGE(pin, port->DIR.reg, pinStr);
+		GPIO_SetPinDirection(port, pin, GPIO_IN);
+		TEST_ASSERT_BIT_LOW_MESSAGE(pin, port->DIR.reg, pinStr);
+	}
+	
+	TEST_ASSERT_BITS_LOW(implementedPins[1], port->DIR.reg);
+};
+
+void t_setPortsAsInput(void){
+	const uint32_t implementedPins[3] = PORT_PIN_IMPLEMENTED;
+	GPIO_SetPortDirection(GPIOA, 0xFFFFFFFF, GPIO_IN);
+	TEST_ASSERT_BITS_LOW(implementedPins[0], GPIOA->DIR.reg);
+
+	GPIO_SetPortDirection(GPIOB, 0xFFFFFFFF, GPIO_IN);
+	TEST_ASSERT_BITS_LOW(implementedPins[1], GPIOB->DIR.reg);
+}
+
+void t_setPortsAsOutput(void){
+	const uint32_t implementedPins[3] = PORT_PIN_IMPLEMENTED;
+	GPIO_SetPortDirection(GPIOA, 0xFFFFFFFF, GPIO_OUT);
+	TEST_ASSERT_BITS_HIGH(implementedPins[0], GPIOA->DIR.reg);
+
+	GPIO_SetPortDirection(GPIOB, 0xFFFFFFFF, GPIO_OUT);
+	TEST_ASSERT_BITS_HIGH(implementedPins[1], GPIOB->DIR.reg);
+}
+
+void t_randomPinDirections(void){
+	srand(time(NULL));
+	const uint32_t implementedPins[3] = PORT_PIN_IMPLEMENTED;
+	for(int i = 0; i < 100; i++){
+		uint8_t portNum = rand() % 2;
+		uint32_t mask = rand() && implementedPins[portNum];
+		enum GPIO_PinDirections direction = rand() % 2? GPIO_OUT : GPIO_IN;
+		GPIO_SetPortDirection(&PORT->Group[portNum], mask, direction);
+		
+		switch(direction){
+			case GPIO_OUT:
+				TEST_ASSERT_BITS_HIGH(mask, PORT->Group[portNum].DIR.reg);
+				break;
+			case GPIO_IN:
+				TEST_ASSERT_BITS_LOW(mask, PORT->Group[portNum].DIR.reg);
+				break;
+		}
+	}
+}
+
+void t_resetPort(void){
+	GPIO_ResetPort(GPIOB);
+	uint32_t defaultDir[3] = PORT_DIR_DEFAULT_VAL;
+	TEST_ASSERT_EQUAL_HEX32(defaultDir[1], REG_PORT_DIR1);
+	
+	uint32_t defaultOut[3] = PORT_OUT_DEFAULT_VAL;
+	TEST_ASSERT_EQUAL_HEX32(defaultOut[1], REG_PORT_OUT1);
+	
+	for(int i = 0; i < 16; i++){
+		TEST_ASSERT_EQUAL_HEX32(0, GPIOB->PMUX[i].reg);
+	}
+	
 }
--- testing/UnitTest/src/GPIO_tests.h
+++ testing/UnitTest/src/GPIO_tests.h
@@ -8,11 +8,24 @@
 #ifndef GPIO_TESTS_H_
 #define GPIO_TESTS_H_
 
+#include <stdlib.h>
+#include <time.h>
+
 #include "unity.h"
+
+void t_pinToMask(void);
 
 void test_myPassingTest(void);
 void test_myFailingTest(void);
 
+void t_setPinsAsInput(void);
+void t_setPinsAsOutput(void);
 
+void t_setPortsAsInput(void);
+void t_setPortsAsOutput(void);
+
+void t_randomPinDirections(void);
+
+void t_resetPort(void);
 
 #endif /* GPIO_TESTS_H_ */
