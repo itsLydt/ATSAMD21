1e4cf9ac9d0cc67ce19d0981aca75c93a512b4c6
itsLydt <lklaus.dev@gmail.com>
2025-01-02 12:43:12 -0600

    added pin assignment configuration file

---
+++ examples/RGB_LED/config_led.h
@@ -0,0 +1,21 @@
+/*
+ * config_led.h
+ *
+ * Created: 1/2/2025 12:41:52 PM
+ *  Author: lydia.klaus
+ */ 
+
+
+#ifndef CONFIG_LED_H_
+#define CONFIG_LED_H_
+
+#include "gpio.h"
+
+/* port and pin assignments for RGB LED */
+PortGroup* const LED_PORT = GPIOB;
+const uint8_t LED_RED = 4;
+const uint8_t LED_GREEN = 5;
+const uint8_t LED_BLUE = 6;
+
+
+#endif /* CONFIG_LED_H_ */
