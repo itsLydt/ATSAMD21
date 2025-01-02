63ffb794d04fe9791a1d32e1971f04363a30185b
itsLydt <lklaus.dev@gmail.com>
2025-01-02 12:58:35 -0600

    defined colors

--- examples/RGB_LED/rgb.c
+++ examples/RGB_LED/rgb.c
@@ -8,6 +8,13 @@
 #include "rgb.h"
 
 const struct LEDColor_t OFF = {};
+const struct LEDColor_t RED = {.r=1};
+const struct LEDColor_t GREEN = {.g=1};
+const struct LEDColor_t BLUE = {.b=1};
+const struct LEDColor_t YELLOW = {.r=1,.g=1};
+const struct LEDColor_t VIOLET = {.r=1,.b=1};
+const struct LEDColor_t TEAL = {.g=1,.b=1};
+const struct LEDColor_t WHITE = {.r=1,.b=1,.g=1};
 
 void LED_Init(){
 	uint32_t pin_mask = (1 << LED_RED) | (1 << LED_GREEN) | (1 << LED_BLUE);
