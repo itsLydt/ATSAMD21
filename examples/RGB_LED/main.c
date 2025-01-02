5dc2eb97727387253c2aa9b3cb3e2a417bcd295e
itsLydt <lklaus.dev@gmail.com>
2025-01-02 11:49:02 -0600

    skeleton project for RBG LED example

---
+++ examples/RGB_LED/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 1/2/2025 11:34:59 AM
+ * Author : itsLydt
+ */ 
+
+
+#include "sam.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
